49f354c6511425b45b2627963f41e875c863a570
gimanjeon <giman.jeon88@gmail.com>
2018-03-24 18:33:44 +0900

    Exception handling for JSONObject class

--- src/android/KakaoPlugin.java
+++ src/android/KakaoPlugin.java
@@ -28,7 +28,6 @@ import android.content.pm.PackageManager.NameNotFoundException;
  * This class echoes a string called from JavaScript.
  */
 public class KakaoPlugin extends CordovaPlugin {
-    @Override
     public boolean execute(String action, JSONObject args, CallbackContext callbackContext) throws JSONException {
 
         if (action.equals("sendTemplate")) {
@@ -54,52 +53,55 @@ public class KakaoPlugin extends CordovaPlugin {
     public void sendTemplate(JSONObject args) {
         Context context = this.cordova.getActivity().getApplicationContext();
 
-        JSONObject content = args.getJsonObject("content");
-        JSONObject link = content.getJsonObject("link");
-        JSONObject social = args.getJsonObject("social");
+        try {
+            JSONObject content = args.getJSONObject("content");
+            JSONObject link = content.getJSONObject("link");
+            JSONObject social = args.getJSONObject("social");
 
-        String title = content.getString("title");
-        String descripttion = content.getString("description");
-        String imageSrc = content.getString("imageUrl");
-        String webUrl = link.getString("webUrl");
-        String mobileWebUrl = link.getString("mobileWebUrl");
+            String title = content.getString("title");
+            String description = content.getString("description");
+            String imageSrc = content.getString("imageUrl");
+            String webUrl = link.getString("webUrl");
+            String mobileWebUrl = link.getString("mobileWebUrl");
 
-        int likeCount = social.getInt("likeCount");
-        int commentCount = social.getInt("commentCount");
+            int likeCount = social.getInt("likeCount");
+            int commentCount = social.getInt("commentCount");
 
-        Log.d("KakaoPlugin", "sendTemplate");
-        Log.d("KakaoPlugin", getKeyHash(context));
-        FeedTemplate params = FeedTemplate
-        .newBuilder(
-            ContentObject.newBuilder(
-                title,
-                imageSrc,
-                LinkObject.newBuilder().setWebUrl(webUrl)
-                .setMobileWebUrl(mobileWebUrl).build())
-        .setDescrption(description.subString(0,25))
-        .build())
-        .setSocial(SocialObject.newBuilder().setLikeCount(likeCount).setCommentCount(commentCount)
-                .setSharedCount(1).setViewCount(2).build())
-        .addButton(new ButtonObject("웹에서 보기", LinkObject.newBuilder().setWebUrl("'https://developers.kakao.com").setMobileWebUrl("'https://developers.kakao.com").build()))
-        .addButton(new ButtonObject("앱에서 보기", LinkObject.newBuilder()
-                .setWebUrl("'https://developers.kakao.com")
-                .setMobileWebUrl("'https://developers.kakao.com")
-                .setAndroidExecutionParams("key1=value1")
-                .setIosExecutionParams("key1=value1")
-                .build()))
-        .build();
+            FeedTemplate params = FeedTemplate
+            .newBuilder(
+                ContentObject.newBuilder(
+                    title,
+                    imageSrc,
+                    LinkObject.newBuilder().setWebUrl(webUrl)
+                    .setMobileWebUrl(mobileWebUrl).build())
+            .setDescrption(description.substring(0,25))
+            .build())
+            .setSocial(SocialObject.newBuilder().setLikeCount(likeCount).setCommentCount(commentCount)
+                    .setSharedCount(1).setViewCount(2).build())
+            .addButton(new ButtonObject("웹에서 보기", LinkObject.newBuilder().setWebUrl("'https://developers.kakao.com").setMobileWebUrl("'https://developers.kakao.com").build()))
+            .addButton(new ButtonObject("앱에서 보기", LinkObject.newBuilder()
+                    .setWebUrl("'https://developers.kakao.com")
+                    .setMobileWebUrl("'https://developers.kakao.com")
+                    .setAndroidExecutionParams("key1=value1")
+                    .setIosExecutionParams("key1=value1")
+                    .build()))
+            .build();
 
-        KakaoLinkService.getInstance().sendDefault(context, params, new ResponseCallback<KakaoLinkResponse>() {
-            @Override
-            public void onFailure(ErrorResult errorResult) {
-                Log.d("KakaoPlugin", errorResult.toString());
-            }
+            KakaoLinkService.getInstance().sendDefault(context, params, new ResponseCallback<KakaoLinkResponse>() {
+                @Override
+                public void onFailure(ErrorResult errorResult) {
+                    Log.d("KakaoPlugin", errorResult.toString());
+                }
 
-            @Override
-            public void onSuccess(KakaoLinkResponse result) {
-                Log.d("KakaoPlugin", "Success");
-            }
-        });
+                @Override
+                public void onSuccess(KakaoLinkResponse result) {
+                    Log.d("KakaoPlugin", "Success");
+                }
+            });
+        } catch(JSONException e) {
+            Log.d("KakaoPlugin", "json error" + e);
+        }
+
     }
 
     public static String getKeyHash(final Context context) {
--- www/cordova-kakao-ec.js
+++ www/cordova-kakao-ec.js
@@ -1,8 +1,8 @@
 var exec = require('cordova/exec');
 
 var Kakao = {
-    sendTemplate: function (success, error, arg0) {
-        exec(success, error, 'Kakao', 'sendTemplate', [arg0]);
+    sendTemplate: function (success, error, arg) {
+        exec(success, error, 'Kakao', 'sendTemplate', arg);
     }
 }
 
